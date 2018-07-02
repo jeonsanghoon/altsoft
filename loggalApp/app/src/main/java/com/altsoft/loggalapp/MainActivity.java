8c05cdb60ac847c702bbb1aa661a09decbb544cc
전상훈 <mrc0700@hotmail.com>
2018-07-02 16:53:02 +0900

    초기 푸쉬

---
+++ loggalApp/app/src/main/java/com/altsoft/loggalapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.altsoft.loggalapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    TabFragment1 tab1;
+    TabFragment2 tab2;
+    TabFragment3 tab3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar= (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        tab1 = new TabFragment1();
+        tab2 = new TabFragment2();
+        tab3 = new TabFragment3();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, tab1).commit();
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("통화기록"));
+        tabs.addTab(tabs.newTab().setText("스팸기록"));
+        tabs.addTab(tabs.newTab().setText("연락처"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("MainActivity", "선택된 탭 : " + position);
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = tab1;
+                } else if (position == 1) {
+                    selected = tab2;
+                } else if (position == 2) {
+                    selected = tab3;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+
+    }
+}
---
+++ loggalApp/app/src/main/java/com/altsoft/loggalapp/TabFragment3.java
@@ -0,0 +1,27 @@
+package com.altsoft.loggalapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link TabFragment3.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TabFragment3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TabFragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_fragment3, container, false);
+    }
+}
